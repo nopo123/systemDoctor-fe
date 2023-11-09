6804ce50e5b86eb2cfb49138400b31a477658096
Karas Patrik <patrik.karas@extern.a1.at>
2023-11-09 16:10:54 +0100

    updated package.json

--- system-doctor-fe/src/pages/ListPatient.js
+++ system-doctor-fe/src/pages/ListPatient.js
@@ -1,9 +1,30 @@
-import React from 'react'
 import Button from '@mui/material/Button'
 import { useSnackbar } from 'notistack'
 import { useNavigate } from 'react-router-dom'
 import PrivateKeyDialog from '../components/PrivateKeyDialog'
 import useAuth from '../hooks/useAuth'
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 const ListPatient = () => {
   const { enqueueSnackbar } = useSnackbar()
@@ -62,6 +83,36 @@ const ListPatient = () => {
           Update private key
         </Button>
       </div>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Birth Id</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">First Name</TableCell>
+            <TableCell align="right">Address</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right"><DeleteIcon /><EditIcon /></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   )
 }
